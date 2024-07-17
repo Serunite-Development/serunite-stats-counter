570bbdaef2cd465f1f550270aca061ac1b28393a
Noah <noahdevelopsthings@gmail.com>
2024-07-17 01:47:07 -0300

    globalStats

--- index.js
+++ index.js
@@ -1,90 +1,141 @@
 const axios = require('axios');
-const fs = require("fs");
+const fs = require('fs');
 
-const inputGames = require("./input_games.json")
+const inputGames = require('./input_games.json');
+const { start } = require('repl');
 
 // we need to also use .get on each takes owner, and repo
 
+const currentDate = new Date();
+const pastTimestamp = currentDate.getTime() - 7 * 24 * 60 * 60 * 1000;
+const lastWeekTimestamp = new Date(pastTimestamp);
+
+console.log(currentDate.toDateString(), lastWeekTimestamp.toDateString());
 
 const promises = [];
 let gameStats = {};
+let weeklyPlaytime = 0;
+let weeklyVisits = 0;
+let totalVisits = 0;
+let totalFavorites = 0;
 for (let gameName in inputGames) {
-    const assetId = inputGames[gameName]
+	const assetId = inputGames[gameName];
 
-    console.log(`Retrieving info for ${gameName} with ID of: ${assetId}`)
+	console.log(`Retrieving info for ${gameName} with ID of: ${assetId}`);
 
-    const promise = axios.get(`https://www.roblox.com/places/api-get-details?assetId=${assetId}`).then((response) => {
-        const data = response.data
+	const promise = axios
+		.get(`https://www.roblox.com/places/api-get-details?assetId=${assetId}`)
+		.then((response) => {
+			const data = response.data;
 
-        const likes = data["TotalUpVotes"]
-        const dislikes = data["TotalDownVotes"]
+			const likes = data['TotalUpVotes'];
+			const dislikes = data['TotalDownVotes'];
 
-        const likeRatio = Math.round((likes/(likes+dislikes)) * 100)
- 
-        gameStats[gameName] = {"CCU": data["OnlineCount"], "Visits": data["VisitedCount"], "Ratio": likeRatio}
-    }).catch((error) => {
-      console.error(`Error retrieving info for ${gameName}:`, error);
-  });;
+			const likeRatio = Math.round((likes / (likes + dislikes)) * 100);
 
-    promises.push(promise)
+			totalVisits += data['VisitedCount'];
+			totalFavorites += data['FavoritedCount'];
+
+			gameStats[gameName] = {
+				CCU: data['OnlineCount'],
+				Visits: data['VisitedCount'],
+				Ratio: likeRatio,
+			};
+		})
+		.catch((error) => {
+			console.error(`Error retrieving info for ${gameName}:`, error);
+		});
+
+	const lastWeekTimestampMonth = lastWeekTimestamp.getMonth() + 1;
+	const currentWeekMonth = lastWeekTimestamp.getMonth() + 1; // 0 indexing D:
+
+	const startRange = `${lastWeekTimestamp.getFullYear()}-${
+		(lastWeekTimestampMonth < 10 ? '0' : '') + lastWeekTimestampMonth
+	}-${
+		(lastWeekTimestamp.getDate() < 10 ? '0' : '') + lastWeekTimestamp.getDate()
+	}`;
+	const endRange = `${currentDate.getFullYear()}-${
+		(currentWeekMonth < 10 ? '0' : '') + currentWeekMonth
+	}-${(currentDate.getDate() < 10 ? '0' : '') + currentDate.getDate()}`;
+
+	promises.push(
+		axios
+			.get(
+				`https://romonitorstats.com/api/v1/charts/get?name=session-length&placeId=${assetId}&timeslice=day&proVersion=false&includeExperienceName=true&start=${startRange}&ends=${endRange}`
+			)
+			.then((response) => {
+				const sessionData = response.data[0].data;
+
+				for (let date in sessionData) {
+					const time = sessionData[date];
+					weeklyPlaytime += time;
+				}
+			})
+	);
+
+	promises.push(
+		axios
+			.get(
+				`https://romonitorstats.com/api/v1/charts/get?name=visits&placeId=${assetId}&timeslice=day&proVersion=false&includeExperienceName=true&start=${startRange}&ends=${endRange}`
+			)
+			.then((response) => {
+				const sessionData = response.data[0].data;
+
+				for (let date in sessionData) {
+					const visits = sessionData[date];
+					weeklyVisits += visits;
+				}
+			})
+	);
+
+	promises.push(promise);
 }
 
-Promise.all(promises).then(() => {
-  console.log(gameStats);
+Promise.all(promises)
+	.then(() => {
+		console.log(gameStats);
+		weeklyVisits = weeklyVisits / 7;
+		weeklyPlaytime = weeklyPlaytime / 7;
 
-  const jsonString = JSON.stringify(gameStats, null, 2); // Pretty print with 2-space indentation
+		let monthlyAverageHours = Math.round(
+			(weeklyPlaytime * weeklyVisits * 30) / 60
+		);
 
-  // Write the JSON string to a file
-  fs.writeFile('gameStats.json', jsonString, (err) => {
-      if (err) {
-          console.error('Error writing to file', err);
-      } else {
-          console.log('Successfully wrote gameStats to gameStats.json');
-      }
-  });
-}).catch((error) => {
-  console.error('Error in Promise.all:', error);
-});
+		console.log(`Monthly average playtime (HOURS): ${monthlyAverageHours}`);
+		console.log(`Total Favorites: ${totalFavorites}`);
+		console.log(`Total Visits: ${totalVisits}`);
 
-// axios.get("https://api.github.com/users/noahrepublic/repos").then((response) => {
-//     const repos = response.data;
- 
-//     repos.forEach((repo) => {
-//         function request(response) {
-//             const length = response.data.length;
-//             console.log(response.data)
-//             console.log(response.data[length - 1])
+		const jsonString = JSON.stringify(gameStats, null, 2); // Pretty print with 2-space indentation
 
-//             if (typeof response.data[length - 1].linesOfCode === 'number') {
-//                 linesWrote += parseInt(response.data[length - 1].linesOfCode);
-//             }
+		// Write the JSON string to a file
+		fs.writeFile('gameStats.json', jsonString, (err) => {
+			if (err) {
+				console.error('Error writing to file', err);
+			} else {
+				console.log('Successfully wrote gameStats to gameStats.json');
+			}
+		});
 
-//             if (typeof linesWrote === 'number') {
-//                 fs.writeFile("linesWrote.txt", linesWrote.toString(), (err) => {
-//                     if (err) {
-//                         console.log(err);
-//                     }
-//                 });
-//             }
-
-//             console.log(linesWrote)
-//         }
-
-//         function onFail() {
-//             console.log("Failed to get " + repo.full_name)
-
-//             promises.push(promiseSleep(60*1000).then(() => {
-//                 console.log("Retrying " + repo.full_name)
-//                 const retryPromise = axios.get(`https://api.codetabs.com/v1/loc?github=${repo.full_name}`).then(request).catch(onFail);
-
-//                 promises.push(retryPromise);
-//             }))
-//         }
-
-//         console.log("Requesting" + repo.full_name)
-//         const promise =  axios.get(`https://api.codetabs.com/v1/loc?github=${repo.full_name}`).then(request).catch(onFail);
-
-//         promises.push(promise);
-//         sleep(10 * 1000)
-//     });
-// });
+		fs.writeFile(
+			'globalStats.json',
+			JSON.stringify(
+				{
+					TotalVisits: totalVisits,
+					TotalFavorites: totalFavorites,
+					MonthlyAverageHours: monthlyAverageHours,
+				},
+				null,
+				2
+			),
+			(err) => {
+				if (err) {
+					console.error('Error writing to file', err);
+				} else {
+					console.log('Successfully wrote globalStats to globalStats.json');
+				}
+			}
+		);
+	})
+	.catch((error) => {
+		console.error('Error in Promise.all:', error);
+	});
